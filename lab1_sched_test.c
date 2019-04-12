93403f32693be9f7d92b124943affd8938bc3f99
kevin0309 <32089450+kevin0309@users.noreply.github.com>
2019-04-13 03:06:33 +0900

    dd

--- lab1_sched_test.c
+++ lab1_sched_test.c
@@ -97,24 +97,25 @@ int calcTotalProcessTime(int data[][2],int col) {
 int main(int argc, char *argv[]){
 	// input ---> int arr[][2] = {{1,2},{{process arrival time},{service time}}}
 	int testData[5][2] = {{0,3},{2,6},{6,5},{8,2},{4,4}};
-	int testData4[5][2] = {{0,3},{2,6},{6,5},{8,2},{4,4}};
-	int testData2[3][2]={{0,6},{3,5},{7,2}};
+	//int testData4[5][2] = {{0,3},{2,6},{6,5},{8,2},{4,4}};
+	//int testData2[3][2]={{0,6},{3,5},{7,2}};
 	
-	int fcfsResSize;
-	int *fcfs= calcFCFS(testData4,5, &fcfsResSize);
-	printResult(testData4,fcfs,5,fcfsResSize);
+	//int fcfsResSize;
+	//int *fcfs= calcFCFS(testData4,5, &fcfsResSize);
+	//printResult(testData4,fcfs,5,fcfsResSize);
 	int rrResSize;
 	int *rr= calcRR(testData, 5, 1, &rrResSize);
 	printResult(testData,rr,5,rrResSize);
-	//int testData[5][2] = {{0, 3}, {2, 6}, {4, 4}, {6, 5}, {8, 2}};
-	//int mlfqResSize;
-	//int *mlfq = calcMLFQ(testData, 5, 1, 5, &mlfqResSize);
-	//printResult(testData, mlfq, 5, mlfqResSize);
 	
-	int testData3[8][2] = {{4, 8}, {2, 6}, {4, 4}, {6, 5}, {8, 2}, {29, 8}, {32, 1}, {33, 6}};
+	int testData2[5][2] = {{0, 3}, {2, 6}, {4, 4}, {6, 5}, {8, 2}};
 	int mlfqResSize;
-//	int *mlfq = calcMLFQ(testData3, 8, 1, 3, &mlfqResSize);
-//	printResult(testData3, mlfq, 8, mlfqResSize);
+	int *mlfq = calcMLFQ(testData2, 5, 1, 3, &mlfqResSize);
+	printResult(testData2, mlfq, 5, mlfqResSize);
+	
+	/*int testData3[8][2] = {{4, 8}, {2, 6}, {4, 4}, {6, 5}, {8, 2}, {29, 8}, {32, 1}, {33, 6}};
+	int mlfqResSize;
+	int *mlfq = calcMLFQ(testData3, 8, 1, 1, &mlfqResSize);
+	printResult(testData3, mlfq, 8, mlfqResSize);*/
 	
 	//mlfq = calcMLFQ(testData, 5, 2, 3, &mlfqResSize);
 	//printResult(testData, mlfq, 5, mlfqResSize);
@@ -159,8 +160,8 @@ int* calcMLFQ(int data[][2], int col, int timeQuantum, int queueSize, int *resSi
 	//Analyze input data.
 	int lastArriveTime = 0; //last arrive time between processes
 	int firstArriveTime = data[0][0]; //first arrive time between processes
-	int totalServiceTime = 0; //total process service time from first process arrival	
-	int *leftServiceTimeArr = malloc(sizeof(int) * col); //remaining service time temporary storage of processes
+	int totalServiceTime = 0; //minimum total process service time from first process arrival	
+	int *leftServiceTimeArr = malloc(sizeof(int) * col); //remaining service time temporary storage for each process
 	for (int i = 0; i < col; i++) {
 		if (data[i][0] > lastArriveTime)
 			lastArriveTime = data[i][0];
@@ -178,7 +179,7 @@ int* calcMLFQ(int data[][2], int col, int timeQuantum, int queueSize, int *resSi
 	
 	//Main process roof start. Exit when all the processes are finished.
 	int procTime = 0; //CPU process timer
-	int curProc = -1; //the process currently being executed by CPU
+	int curProc; //the process currently being executed by CPU
 	int quantumTimer = timeQuantum; //time quantum counter
 	while (getLeftTime(leftServiceTimeArr, col) > 0) {
 		/////
@@ -199,7 +200,7 @@ int* calcMLFQ(int data[][2], int col, int timeQuantum, int queueSize, int *resSi
 		
 		//When a new process is scheduled, if there is a process that was previously running, drop it to the queue below.
 		if (pushCnt != -1) {
-			if (procTime > firstArriveTime && qSize(&queueList[0]) > data[pushCnt][1])
+			if (qSize(&queueList[0]) > data[pushCnt][1])
 				while (qPeek(&queueList[0]) != pushCnt)
 					qPush(&queueList[1], qPop(&queueList[0]));
 			quantumTimer = timeQuantum;
@@ -231,23 +232,21 @@ int* calcMLFQ(int data[][2], int col, int timeQuantum, int queueSize, int *resSi
 		/////
 		
 		//When there are remaining jobs and there are another processes scheduled in scheduler, drop it to the queue below.
-		int latestProc = result[procTime];
 		int totalQueueSize = 0;
 		for (int i = 0; i < queueSize; i++)
 			totalQueueSize += qSize(queueList+i);
 		for (int i = 0; i < queueSize; i++)
 			if (qSize(&queueList[i]) > 0) {
-				if (latestProc == qPeek(&queueList[i]))
+				if (curProc == qPeek(&queueList[i]))
 					quantumTimer--;
 				else
 					quantumTimer = timeQuantum;
-				if (quantumTimer <= 0 && totalQueueSize != leftServiceTimeArr[latestProc]) {
-					if (i != queueSize - 1)
-						for (int j = 0; j < leftServiceTimeArr[latestProc]; j++)
+				if (quantumTimer <= 0 && totalQueueSize != leftServiceTimeArr[curProc]) {
+					for (int j = 0; j < leftServiceTimeArr[curProc]; j++)
+						if (i != queueSize - 1)
 							qPush(&queueList[i+1], qPop(&queueList[i]));
-					else
-						for (int j = 0; j < leftServiceTimeArr[latestProc]; j++)
-							qPush(&queueList[i], qPop(&queueList[i])); //Push to itself because there is no queue below.
+						else //Push to itself because there is no queue below.
+							qPush(&queueList[i], qPop(&queueList[i]));
 					quantumTimer = timeQuantum;
 				}
 				break;
@@ -260,7 +259,6 @@ int* calcMLFQ(int data[][2], int col, int timeQuantum, int queueSize, int *resSi
 		
 		procTime++;
 	}
-	free(leftServiceTimeArr);
 	*resSize = procTime;
 	return result;
 }
